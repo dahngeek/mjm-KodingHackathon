3640949d84af4080f76ba148957f5212c293f638
Dahngeek <me@dahngeek.com>
2014-12-08 01:55:36 -0600

    Actualizando: Nuevas secciones y Places complete

--- Maquetado/js/main.js
+++ Maquetado/js/main.js
@@ -4,6 +4,18 @@ jQuery(document).ready(function($){
 	  ]);
 	$('#skip').click(function() {
 		$('#lgin').fadeOut();
+		$('#contenidoResearcher').fadeIn();
 		return false;
 	});
+	$('#researcher').click(function() {
+		$('#contenidoPrincipal').fadeOut();
+		$('#logobig').fadeOut();
+		$('#logosmall').fadeIn();
+		$('#lgin').fadeIn();
+		return false;
+	});
+
+	// This example displays an address form, using the autocomplete feature
+// of the Google Places API to help users fill in the information.
+
 });
